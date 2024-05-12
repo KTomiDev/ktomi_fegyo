d19916f309aa5bb9949f6475415eaf919e50c62c
Tomi <168299301+KTomiDev@users.noreply.github.com>
2024-05-12 18:48:48 +0200

    :)
    :)

---
+++ README.md
@@ -0,0 +1,6 @@
+***Egyszerű job, és ammo mutatás.***
+
+Ha fegyver van a kezedben elég sokat fogyaszt, ezt a scriptet régebben írtam még, akkor még nem tudtam megoldani máshogy, mert nem értettem annyira a script íráshoz.
+Szóval nyugodt szívvel osztom meg mindenkivel.
+
+**KTOMI**
---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+--[[ FX ]]--
+fx_version   'cerulean'
+lua54        'yes'
+game         'gta5'
+
+--[[ Név, stb ]]--
+name         'ktomi_fegyo'
+version      '0.0.0'
+author       'K.Tomi0714'
+description   'Egyszerű ammo és job mutató.'
+
+--[[ Manifest ]]--
+client_scripts {
+	'client.lua',
+}
